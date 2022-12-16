b06d66bcb9771b075c0d8b3c485721c5c88eec69
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-16 22:02:04 +0530

    Added Logical Programming Program

---
+++ Day_6LogicalProgramming/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day_6LogicalProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Diaplaying Logical Programming");
+            Console.WriteLine("------------------------------");
+            FibonacciSeries series = new FibonacciSeries();
+            series.DisplayFibbonacci();
+            Console.WriteLine("------------------------------");
+            PerfectNumber.DisplayPerfectnumber();
+            Console.WriteLine("------------------------------");
+            PrimeNumber.Prime();
+            Console.WriteLine("------------------------------");
+            ReverseNumber reverseNumber = new ReverseNumber();
+            reverseNumber.DisplayReverse();
+            Console.WriteLine("------------------------------");
+            CouponsNumber.Coupon(3);
+            Console.WriteLine("------------------------------");
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.Dispaly();
+
+
+
+        }
+    }
+}
